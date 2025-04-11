11650f2a65a5bd888bec2a6f421bf9c2b433f302
Mehmonov-Sherzod <mehmovovsherzod@gmail.com>
2025-04-11 17:01:12 +0500

    bolindi

--- pdpgithub/Program.cs
+++ pdpgithub/Program.cs
@@ -7,7 +7,7 @@
         int number1 =Convert.ToInt32(Console.ReadLine());
         int number2 =Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine(number1-number2);
+        Console.WriteLine(number1/number2);
 
 
 
